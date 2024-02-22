5f80eaa8a019b4f3bbab4c7f889b001d83a18f72
Nayuki <Kuuuuuuuu@users.noreply.github.com>
2024-02-22 23:09:08 +0700

    Various Update! :smile:

--- src/main/java/com/nayuki/dto/ResponseDto.java
+++ src/main/java/com/nayuki/dto/ResponseDto.java
@@ -5,22 +5,17 @@ import lombok.Getter;
 /**
  * ResponseDto is a values container for presenting response info
  *
+ * @param message Response message field
  * @author Rudolf Barbu, Nayuki
  * @version 1.0.0
  */
 @Getter
-public final class ResponseDto {
-    /**
-     * Response message field
-     */
-    private final String message;
-
+public record ResponseDto(String message) {
     /**
      * Setter for message field
      *
      * @param message message to display
      */
-    public ResponseDto(String message) {
-        this.message = message;
+    public ResponseDto {
     }
 }
